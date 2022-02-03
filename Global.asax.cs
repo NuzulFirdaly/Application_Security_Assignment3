fbbe641186b6f4213956adbf8f8b448d6c3aeb02
NuzulFirdaly <nuzul5567@gmail.com>
2022-02-03 19:56:29 +0800

    Fixed Missing X-Frame-Options HTTP header

--- Global.asax.cs
+++ Global.asax.cs
@@ -17,5 +17,9 @@ namespace Application_Security_Assignment
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.AddHeader("X-Frame-Options", "DENY");
+        }
     }
 }
